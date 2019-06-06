989d8b593df3e2d975bad96f19ea5671c3991dce
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-05 22:45:02 -0700

    refactor getSwansonQuote() to seperate concerns, set up sessionStorage for a single player score

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 import './styles.css';
 import { getSwansonQuote, getTriva } from './api-calls';
-import { buildAnswers, checkAnswer } from './functions';
+import { checkAnswer, displayAnswers, displayQuestion } from './functions';
 import Ron1 from './../src/img/Ron1.png';
 
-let score = 0;
-let questionNumber;
-
+if(!sessionStorage.getItem('score')) {
+  sessionStorage.setItem('score', 0);
+}
 const myRon = new Image();
 myRon.src = Ron1;
 
@@ -16,39 +16,34 @@ $(document).ready(function(){
   $('#host').prepend(myRon);
 
   getSwansonQuote().then((response) => {
-    $('#swansonQuotes').append(response + " Ok.  Next Question.");
+    $('#host-text').append(response + " Ok.  Next Question.");
   });
 
-  
   getTriva().then(response => {
-    let incorrectAnswers = response["results"][0]["incorrect_answers"];
-    correctAnswer = response['results'][0]["correct_answer"];
-    let answers = buildAnswers(correctAnswer, incorrectAnswers);
-    $('#questions').text(response["results"][0]["question"].replace(/&quot;/g,'"').replace(/&#039;/g, "'"));
 
-    let counter =1;
-    answers.forEach(answer => {
-      $('#answers').append('<li data-id="' + counter  + '" class="answerBox">' + answer.replace(/&quot;/g,'"').replace(/&#039;/g, "'") + '</li>');
-      counter++;
-    });
-    
+    displayQuestion(response);
+    correctAnswer = displayAnswers(response);
+
+
     // localStorage.setItem("questionNumber", (stored + 1));
     localStorage.questionNumber = Number(localStorage.questionNumber) + 1;
 
-    $("#total").text(localStorage.getItem("questionNumber"));
   });
-
+  
   $("#answers").on("click", "li", function(event) {
     event.preventDefault();
     let userAnswer = $(this).text();
     // let strId = this.getAttribute("data-id");
     // console.log(str, strId, correctAnswer);
-
+    
     let isCorrect  = checkAnswer(userAnswer, correctAnswer);
     console.log(isCorrect);
     if(isCorrect) {
-      score+=1;
-      localStorage.setItem("score", score);
+      // sessionStorage.clear();
+      let oldScore = Number(sessionStorage.getItem('score'));
+      console.log(oldScore);
+      sessionStorage.setItem('score', (oldScore + 1));
+      $("#playerScore").text(sessionStorage.getItem('score'));
     }
   });
 
