d0a2b6ab8b31991af0e63e4cfb8a834674205238
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-05 22:43:48 -0700

    add displayAnswers() and displayQuestion()

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,27 @@
+import $ from 'jquery';
+
 export function buildAnswers(string, array){
-  let index = Math.floor(Math.random() * 3); 
+  let index = Math.floor(Math.random() * 3);
   array.splice(index, 0, string);
   return array;
 }
 
 export function checkAnswer(userAnswer, correctAnswer){
   return (userAnswer == correctAnswer);
+}
+
+export function displayAnswers(responseObj) {
+  let incorrectAnswers = responseObj["results"][0]["incorrect_answers"];
+  let correctAnswer = responseObj['results'][0]["correct_answer"];
+  let answers = buildAnswers(correctAnswer, incorrectAnswers);
+  let counter = 1;
+  answers.forEach(answer => {
+    $('#answers').append('<li data-id="' + counter  + '" class="answerBox">' + answer.replace(/&quot;/g,'"').replace(/&#039;/g, "'") + '</li>');
+    counter++;
+  });
+  return correctAnswer;
+}
+
+export function displayQuestion(responseObj){
+  $('#host-question').text(responseObj["results"][0]["question"].replace(/&quot;/g,'"').replace(/&#039;/g, "'"));
 }
